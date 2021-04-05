63a512d7cc1f131c2d6d826a0df957daebfdd331
neerajdask <iamneerajdas@gmail.com>
2021-04-05 11:40:22 +0530

    Dynamic navigation. params

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import App from './App.vue';
 
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList }
+    { path: '/users', component: UsersList },
+    { path: '/teams/:id', component: TeamMembers }
   ]
 });
 
